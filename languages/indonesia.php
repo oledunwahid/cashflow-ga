9681812b2a250a42f2b2c6f40812d87fddb69197
Khaled Makkawirelang <makkawirelang@gmail.com>
2025-04-10 10:20:40 +0700

    fix: interface

--- languages/indonesia.php
+++ languages/indonesia.php
@@ -3,9 +3,8 @@ return [
     "dashboard" => "Dasbor",
     "cash_advance" => "Uang Muka",
     "petty_cash_in" => "Kas Masuk",
-	"petty_cash_out" => "Kas Keluar",
+    "petty_cash_out" => "Kas Keluar",
     "user_roles" => "Peran Pengguna",
     "master_data" => "Data Utama",
     "employee" => "Karyawan",
 ];
-?>
